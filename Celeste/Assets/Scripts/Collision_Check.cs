7329cf908b9fedf8aef3434d437d11254110963c
Hayden <haydenhurst64@gmail.com>
2020-11-23 02:37:20 -0600

    Basic Movement

---
+++ Celeste/Assets/Scripts/Collision_Check.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision_Check : MonoBehaviour
+{
+
+    public bool onGround;
+    public bool onWall;
+    public bool onRightWall;
+    public bool onLeftWall;
+    public int wallSide;
+
+    public float collisionRadius = 0.25f;
+    public Vector2 bottomOffset, rightOffset, leftOffset;
+    private Color debugCollisionColor = Color.blue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        onGround = Physics2D.OverlapCircle((Vector2)transform.position + bottomOffset, collisionRadius);
+        onWall = Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, collisionRadius)
+            || Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, collisionRadius);
+        onRightWall = Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, collisionRadius);
+        onLeftWall = Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, collisionRadius);
+
+        wallSide = onRightWall ? -1 : 1;
+    }
+}
